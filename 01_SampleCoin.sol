efd39a3bcb2b55017a9a94793381b210f7ab0357
Jhonattan <jma200@outlook.com>
2018-09-10 22:18:06 -0500

    main

---
+++ 01_SampleCoin.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0; //#A
+
+contract SimpleCoin { //#B
+
+ mapping (address => uint256) public coinBalance; //#C
+
+ constructor() { //#D
+
+  coinBalance[0x14723a09acff6d2a60dcdf7aa4aff308fddc160c] = 10000; //#E
+ }
+
+ function transfer(address _to, uint256 _amount) { //#F
+
+ coinBalance[msg.sender] -= _amount; //#G 
+ coinBalance[_to] += _amount;//#H
+ }
+}
---
+++ 02_SampleCoin.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.0; //#A
+
+contract SimpleCoin { //#B
+
+ mapping (address => uint256) public coinBalance; //#C
+ event Transfer(address indexed from, address indexed to, uint256 value);
+
+ constructor(uint256 _initialSupply) { //#D
+    coinBalance[msg.sender] = _initialSupply;
+ }
+
+    function transfer(address _to, uint256 _amount) {
+        require(coinBalance[msg.sender] >= _amount); 
+        require(coinBalance[_to] + _amount >= coinBalance[_to]); 
+        coinBalance[msg.sender] -= _amount;
+        coinBalance[_to] += _amount;
+        Transfer(msg.sender, _to, _amount);
+    }
+}
